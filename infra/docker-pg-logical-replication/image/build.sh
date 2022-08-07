46af69748ac48d2e29b1de6de4cd6953519abe5d
Kibae Shin <kibae.shin@gmail.com>
2022-08-08 03:04:02 +0900

    Fix: github workflow

--- infra/docker-pg-logical-replication/image/build.sh
+++ infra/docker-pg-logical-replication/image/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t kibaes/postgres-logical-replication-dev:14-dev-20220808 .
+docker buildx build --platform linux/amd64,linux/arm64 -t kibaes/postgres-logical-replication-dev:14-dev-20220810 . --push
