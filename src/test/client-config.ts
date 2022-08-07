62a6bb99e899649adc3cc0a0f411ad8a6cfdf6c4
Kibae Shin <kibae.shin@gmail.com>
2022-08-08 03:41:23 +0900

    Fix: github workflow

--- src/test/client-config.ts
+++ src/test/client-config.ts
@@ -2,15 +2,12 @@ import { ClientConfig } from 'pg';
 
 // infra/docker-pg-logical-replication/docker-compose.yml
 export const TestClientConfig: ClientConfig = {
-  // host: process.env.POSTGRES_HOST || 'localhost',
-  // port: Number(process.env.POSTGRES_PORT || 54320),
-  // user: 'postgres',
-  // password: 'postgrespw',
-  // database: 'playground',
-  connectionString: `postgres://postgres:postgrespw@${process.env.POSTGRES_HOST || 'localhost'}:${Number(
-    process.env.POSTGRES_PORT || 54320
-  )}/playground`,
-  connectionTimeoutMillis: 500,
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: Number(process.env.POSTGRES_PORT || 54320),
+  user: 'postgres',
+  password: 'postgrespw',
+  database: 'playground',
+  // connectionString: `postgres://postgres:postgrespw@${process.env.POSTGRES_HOST || 'localhost'}:${Number(process.env.POSTGRES_PORT || 54320)}/playground`,
 };
 
 console.log(TestClientConfig);
