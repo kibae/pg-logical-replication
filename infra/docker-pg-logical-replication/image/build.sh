a5d7302f7c3155a6d87a379e56afddae44c49afb
Kibae Shin <kibae.shin@gmail.com>
2025-05-12 04:08:06 +0900

    ci: pgsql version

--- infra/docker-pg-logical-replication/image/build.sh
+++ infra/docker-pg-logical-replication/image/build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-docker buildx build --platform linux/amd64,linux/arm64/v8 -t kibaes/postgres-logical-replication-dev:14-dev-20221002 . --push
+cd "$(dirname "$0")" || exit
+
+docker buildx build -f ./Dockerfile-pgsql14 --platform linux/amd64,linux/arm64/v8 -t kibaes/postgres-logical-replication-dev:14 . --push
+docker buildx build -f ./Dockerfile-pgsql15 --platform linux/amd64,linux/arm64/v8 -t kibaes/postgres-logical-replication-dev:15 . --push
+docker buildx build -f ./Dockerfile-pgsql16 --platform linux/amd64,linux/arm64/v8 -t kibaes/postgres-logical-replication-dev:16 . --push
+docker buildx build -f ./Dockerfile-pgsql17 --platform linux/amd64,linux/arm64/v8 -t kibaes/postgres-logical-replication-dev:17 . --push
+
